e3818245411987a86ad9232e4b5d40eb203e30a6
kamfeskaya <chris@devtachom.com>
2022-01-02 11:33:18 +0200

    feat: reset project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import "@fontsource/lexend/latin.css";
 
-import Layout from "components/layout";
-
 import customTheme from "styles/customTheme";
 import "styles/globals.css";
 
@@ -17,9 +15,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 };
